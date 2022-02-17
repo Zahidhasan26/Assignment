9ecf6dd87ba917c189ea6a71d286c501e0bf52e3
Zahid Hasan <zahidhasan2632@gmail.com>
2022-02-17 23:58:20 +0600

    Final

--- js/balance.js
+++ js/balance.js
@@ -11,12 +11,21 @@ function balanceAfterExpense(){
     const balance = parseInt(income) - expenseCost();
     return balance;
 }
-
+   
 // Final Calculate Button
 document.getElementById("calculate-button").addEventListener('click',function(){
+
+    const income = document.getElementById('income-recieved').value;
+    const foodCost = document.getElementById("food-expense").value;
+    const rentCost = document.getElementById("rent-expense").value;
+    const clothesCost = document.getElementById("clothes-expense").value;
+    if (income<0 || foodCost<0||rentCost<0||clothesCost< 0){
+        alert('Please enter positive number only')
+    }
+ else {
     document.getElementById('total-expense').innerText = expenseCost();
     document.getElementById('total-balance').innerText = balanceAfterExpense();
-    
+ }
 })
 
 // Savings section functions 
@@ -32,8 +41,16 @@ function remainingBalance (){
     return remainingBalanceTotal;
 }
 // save button
-document.getElementById("calculate-saving").addEventListener('click',function(){
-    document.getElementById('saving-amount').innerText = savingAmount();
-    document.getElementById('remaining-balance').innerText = remainingBalance();
-    
-})
+    document.getElementById("calculate-saving").addEventListener('click',function(){
+        const income = document.getElementById('income-recieved').value;
+        const saveByPercent = document.getElementById('save-percent').value;
+        const incomeTotal = document.getElementById('income-recieved').value;
+    if (income<0 || saveByPercent<0||incomeTotal<0){
+        alert('Please enter positive number only')
+    }
+        else{
+            document.getElementById('saving-amount').innerText = savingAmount();
+        document.getElementById('remaining-balance').innerText = remainingBalance();
+        }
+        
+    })
